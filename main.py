a46d8ee3304ec999f0e14744344471ca892bd6da
ruiyigan <85063215+ruiyigan@users.noreply.github.com>
2023-01-14 19:19:57 +0800

    tried ray instead of multiprocessor

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from multiprocessing import Process
+import ray
+ray.init()
 
 # after which, whenever they are away from their computer for too long (tracked by face tracker), answerbox will be prompted 
 # regarding one of the questions/ans given by user
@@ -8,15 +10,25 @@ from textbox import *
 from facetracker import *
 from gif import *
 
+@ray.remote
+def func1():
+  Textbox()
+
+@ray.remote
+def func2():
+  face_tracker = FaceTracker(start_time=time.time())
+  face_tracker.start()
+
 
 if __name__=='__main__':
   # when application launch, launch textbok for user to fill in list of things they want to remember
-  p1 = Process(target = Textbox)
-  p1.start()
+  # p1 = Process(target = Textbox)
+  # p1.start()
 
-  # face tracker launches
-  face_tracker = FaceTracker(start_time=time.time())
-  p2 = Process(target = face_tracker.start)
-  p2.start()
+  # # face tracker launches
+  # face_tracker = FaceTracker(start_time=time.time())
+  # p2 = Process(target = face_tracker.start)
+  # p2.start()
+  ray.get([func1.remote(), func2.remote()])
     
 
